1408d679c319b941607a6a55a66de3fa8ca16042
Kyzyma <kyzyma77@gmail.com>
2016-07-12 23:09:25 +0300

    Прикрутил Ninject

---
+++ Weather/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weather.Models;
+using Weather.Services;
+
+namespace Weather.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GetJasonData jsData;
+        IGetJasonData _jsData;
+
+        /* public HomeController()
+             {
+                 jsData = new GetJasonData();
+             }
+        */
+        public HomeController(IGetJasonData jsData)
+        {
+            _jsData = jsData;
+        }
+        // GET: /Home/
+
+        public ActionResult Index()
+            {
+                var ob = _jsData.OutData("london",3);
+                return View(ob);
+            }
+
+            [HttpPost]
+            public ActionResult Index(BaseObject City)
+            {
+                if (City.ChooseCityInput == null || City.ChooseCityInput.Length ==0)
+                {
+                    var ob = _jsData.OutData(City.ChooseCityList, City.CountDays);
+                    
+                    return View(ob);
+                }
+                else
+                {
+                    var ob = _jsData.OutData(City.ChooseCityInput, City.CountDays);
+                    ob.CountDays = City.CountDays;
+                    return View(ob);
+                }
+            }
+
+            
+
+            
+        
+    }
+}
